ae37591623c7ab5d891dbac17e0baa4c3dc83670
New Light <46023692+OrNaishtat@users.noreply.github.com>
2020-01-30 17:08:59 +0200

    Update OrNaishtat_task.sh

--- OrNaishtat_task.sh
+++ OrNaishtat_task.sh
@@ -2,18 +2,12 @@
 ###################################################
 # Author: NewLight (Or Naishtat)
 # Version: 0.1
-# Purpose: Check candidate initial knowledge with linux systems.
-
 # Task 1 - Write a command or script that will perform a file rename for all files ending with '.ext' to the same filename ending with '.newext"
-
 # Task 2 - Please write a command or a script that will use the output of 'ps aux' and show the:
 # + Total amount of CPU used
 # + Total amount of CPU used by non-root users
-
 # Task 3 - the file /etc/ssh/sshd_config was changed on the machine. Which command should be ran in order to apply the changes to the running service?
-
 # Task 4 - Please write a script or command to show which RPM package contains the file '/bin/bash'
-
 # Task 5 - Please write a command or a script to the machine's distribution type, version, and kernel version.
 ###################################################
 
